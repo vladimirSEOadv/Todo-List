9d5a77d2490e1f51bf0e2f6c23565c3a6ffbb041
vladimirSEOadv <vladimirSEOadv@gmail>
2022-06-02 17:43:23 +0300

    Add modal window

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,10 @@ import './styles/App.css'
 import PostList from "./components/PostList";
 import PostForm from "./components/PostForm";
 import PostFilter from "./components/PostFilter";
+import MyModal from "./components/UI/MyModal/MyModal";
+import MyButton from "./components/UI/button/MyButton";
 
-//https://youtu.be/GNrdg3PzpJQ?t=4949
+//https://youtu.be/GNrdg3PzpJQ?t=5427
 //https://github.com/utimur/react-fundamental-course/blob/master/src/hooks/usePosts.js
 
 function App() {
@@ -15,6 +17,7 @@ function App() {
         ]
     )
     const [filter, setFilter] = useState({sort: '', query: ''})
+    const [modal, setModal] = useState(false)
 
     const sortedPosts = useMemo(() => {
         if(filter.sort) {
@@ -29,6 +32,7 @@ function App() {
 
     const createPost = (newPost) => {
         setPosts([...posts, newPost])
+        setModal(false)
     }
 
     const removePost = (post) => {
@@ -37,7 +41,12 @@ function App() {
 
   return (
     <div className="App">
-        <PostForm create={createPost}/>
+        <MyButton style={{marginTop: 30}} onClick={()=>setModal(true)} >
+            Создать пользователя
+        </MyButton>
+        <MyModal visible={modal} setVisible={setModal}>
+            <PostForm create={createPost}/>
+        </MyModal>
         <hr style={{margin: '15px 0'}}/>
         <PostFilter
             filter={filter}
