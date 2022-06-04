1a31902dc72fc7c4433d154e8e44f672b9423fa4
vladimirSEOadv <vladimirSEOadv@gmail>
2022-06-04 22:30:13 +0300

    create usePosts custom hooks

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import PostForm from "./components/PostForm";
 import PostFilter from "./components/PostFilter";
 import MyModal from "./components/UI/MyModal/MyModal";
 import MyButton from "./components/UI/button/MyButton";
+import {usePosts} from "./hooks/usePosts";
 
 //https://youtu.be/GNrdg3PzpJQ?t=5427
 //https://github.com/utimur/react-fundamental-course/blob/master/src/hooks/usePosts.js
@@ -18,17 +19,7 @@ function App() {
     )
     const [filter, setFilter] = useState({sort: '', query: ''})
     const [modal, setModal] = useState(false)
-
-    const sortedPosts = useMemo(() => {
-        if(filter.sort) {
-            return [...posts].sort((a, b) => a[filter.sort].localeCompare(b[filter.sort]))
-        }
-        return posts
-    }, [filter.sort, posts])
-
-    const sortedAdnSearchedPosts = useMemo(()=>{
-        return sortedPosts.filter(post => post.title.toLowerCase().includes(filter.query.toLowerCase()))
-    }, [filter.query, sortedPosts])
+    const sortedAdnSearchedPosts = usePosts(posts, filter.sort, filter.query);
 
     const createPost = (newPost) => {
         setPosts([...posts, newPost])
@@ -41,8 +32,8 @@ function App() {
 
   return (
     <div className="App">
-        <MyButton style={{marginTop: 30}} onClick={()=>setModal(true)} >
-            Создать пользователя
+        <MyButton style={{marginTop: 30}} onClick={() => setModal(true)} >
+            Создать пост
         </MyButton>
         <MyModal visible={modal} setVisible={setModal}>
             <PostForm create={createPost}/>
