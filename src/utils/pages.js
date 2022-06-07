baab70d6a7f860781c9ffe94bba9b717ca0efff2
vladimirSEOadv <vladimirSEOadv@gmail>
2022-06-07 21:56:42 +0300

    Add getPageArray in logic function

--- src/utils/pages.js
+++ src/utils/pages.js
@@ -1,3 +1,11 @@
 export const getPageCount = (totalCount, limit) => {
     return Math.ceil(totalCount / limit)
 }
+
+export const  getPageArray = (totalPages) => {
+    let result = []
+    for (let i = 1; i < totalPages; i++) {
+        result.push(i)
+    }
+    return result
+}
