15f97c535e8a9141e963e686e4055816b747c18b
vladimirSEOadv <vladimirSEOadv@gmail>
2022-06-08 22:59:42 +0300

    refactoring pages.js

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React, {useEffect, useState} from 'react';
+import React, {useEffect, useMemo, useState} from 'react';
 import './styles/App.css'
 import PostList from "./components/PostList";
 import PostForm from "./components/PostForm";
@@ -11,9 +11,7 @@ import Loader from "./components/UI/Loader/Loader";
 import {useFetching} from "./hooks/useFetching";
 import {getPageArray, getPageCount} from "./utils/pages";
 
-
-
-//https://youtu.be/GNrdg3PzpJQ?t=5427
+//https://youtu.be/GNrdg3PzpJQ?t=7515
 
 
 function App() {
@@ -25,7 +23,7 @@ function App() {
     const [page, setPage] = useState(1)
     const sortedAdnSearchedPosts = usePosts(posts, filter.sort, filter.query);
 
-    let pagesArray = getPageArray(totalPages);
+    let pagesArray = getPageArray(totalPages, limit)
 
     const [fetchPosts, isPostLoading, postError] = useFetching(async () => {
         const responce = await PostsService.getAll(limit, page);
--- src/utils/pages.js
+++ src/utils/pages.js
@@ -1,11 +1,18 @@
+import {useMemo} from "react";
+
 export const getPageCount = (totalCount, limit) => {
     return Math.ceil(totalCount / limit)
 }
 
-export const  getPageArray = (totalPages) => {
-    let result = []
-    for (let i = 1; i < totalPages; i++) {
-        result.push(i)
-    }
-    return result
+export const getPageArray = (totalPages, limit) => { // Новая функция
+    const pagesArray = useMemo(() => {
+        console.log('start Memo')
+        let result = []
+        for (let i = 1; i < totalPages; i++) {
+            result.push(i)
+        }
+        return result
+    },[totalPages], [limit])
+    return pagesArray
 }
+
