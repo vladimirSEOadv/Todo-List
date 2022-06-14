cb819f815fdd0fd262e03adc7c61c536a10948e2
vladimirSEOadv <vladimirSEOadv@gmail>
2022-06-14 15:29:50 +0300

    change About page

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,10 +1,15 @@
 import React from 'react';
+import {Link} from "react-router-dom";
 
 const About = () => {
     return (
-            <h1>
+        <div>
+            <div style={{left: 0}}><Link to="/">Главная</Link> / О компании</div>
+            <h1 style={{display:"flex", justifyContent:"center"}}>
                 О кампании
             </h1>
+        </div>
+
     );
 };
 
