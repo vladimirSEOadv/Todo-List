700915c25b227d51d1fa1c8f5c748dce991e92db
vladimirSEOadv <vladimirSEOadv@gmail>
2022-06-16 22:08:42 +0300

    add auth and redirect to login

--- src/components/AppRouter.jsx
+++ src/components/AppRouter.jsx
@@ -1,18 +1,34 @@
 import React from 'react';
 import {Route, Routes} from "react-router-dom";
-import {routs} from "../router/index"
+import {privateRouts, publicRouts} from "../router/index"
 
 const AppRouter = () => {
+    const isAuth = false;
     return (
-        <Routes>
-            {routs.map((route) =>
-                <Route
-                    path={route.path}
-                    element={route.element}
-                    key={route.path}
-                />
-            )}
-        </Routes>
+        isAuth
+            ?
+            <Routes>
+                {privateRouts.map((route) =>
+                    <Route
+                        path={route.path}
+                        element={route.element}
+                        key={route.path}
+                    />
+
+                )}
+                {/*<Navigate to="/posts-page" replace={true} />*/}
+            </Routes>
+            :
+            <Routes>
+                {publicRouts.map((route) =>
+                    <Route
+                        path={route.path}
+                        element={route.element}
+                        key={route.path}
+                    />
+                )}
+
+            </Routes>
     );
 };
 
--- src/router/index.jsx
+++ src/router/index.jsx
@@ -4,11 +4,18 @@ import PostsPage from "../pages/PostsPage";
 import {SinglePostPage} from "../pages/SinglePostPage";
 import PageNotFound from "../pages/PageNotFound";
 import React from "react";
+import Login from "../pages/Login";
 
-export const routs = [
+export const privateRouts = [
     {path: '/', element: <MainPage/>},
     {path: '/about', element: <About/>},
     {path: '/posts-page', element: <PostsPage/>},
     {path: '/posts-page/:id', element: <SinglePostPage/>},
     {path: '*', element: <PageNotFound/>}
+]
+
+export const publicRouts = [
+    {path: '/login', element: <Login/>},
+    {path: '/', element: <Login/>},
+    {path: '*', element: <Login/>}
 ]
